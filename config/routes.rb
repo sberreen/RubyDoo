7ed89bc2abfd85e500086edd70f7ea5f9300d6df
SHANE BERREEN <sberreen@gene.com>
2014-03-19 14:38:59 -0700

    Added Home and About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RubyRubyDoo::Application.routes.draw do
+  get "pages/home"
+  get "about" => "pages#about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
